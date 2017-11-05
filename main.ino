5b72273cdb20916a40c189bd06b06daa68bc8c5a
idowolf <idowolf@gmail.com>
2017-11-05 16:29:56 +0200

    fixed amp in full notes version

--- main.ino
+++ main.ino
@@ -16,12 +16,12 @@ void loop() {
   int amplitudeSelector = analogRead(A2);
   int thisPitch = map(sensorReading, 720, 930, 0, 8);
   int octave  = map(octaveSelector, 0, 1024, 0, 3);
-  int amplitude = map(amplitudeSelector, 0, 1024, 1, 10);
+  int amplitude = map(amplitudeSelector, 0, 1024, 0, 51);
   if(thisPitch > 7) thisPitch = 7;
   if(thisPitch < 0) thisPitch = 0;
   note(arr[thisPitch]);
-  playnote(pow(2, octave));
-  setx(amplitude / 10.0);
+  playnote(pow(2, octave), amplitude);
+  setx();
 }
 
 int curr = arr[0];
@@ -29,8 +29,8 @@ int target = arr[0];
 int mstep;
 double x = 0;
 
-void setx(double amp) {
-  x += amp / PI;
+void setx() {
+  x += 1 / PI;
 }
 
 void note(float key) {
@@ -38,8 +38,8 @@ void note(float key) {
   mstep = abs(curr - target) / 5;
 }
 
-void playnote(int octave) {
-  double y = curr * octave + sin(x) * 10;
+void playnote(int octave, int amp) {
+  double y = curr * octave + sin(x) * amp;
   Serial.println(y);
   tone(9,y, 100);
   if(curr > target && curr - mstep > target)
