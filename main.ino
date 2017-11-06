6cac743ddfbfe3675b77303686e5b09cf87b9b33
idowolf <idowolf@gmail.com>
2017-11-06 15:51:20 +0200

    no notes, raw input

--- main.ino
+++ main.ino
@@ -12,15 +12,12 @@ bool dir;
 
 void loop() {
   int sensorReading = analogRead(A0);
+  Serial.println(sensorReading);
   int wavelengthSelector = analogRead(A1);
   int amplitudeSelector = analogRead(A2);
-  int thisPitch = map(sensorReading, 720, 930, 0, 5);
   int wavelength  = map(wavelengthSelector, 0, 1024, 0, 10);
   int amplitude = map(amplitudeSelector, 0, 1024, 0, 51);
-  if(thisPitch > 4) thisPitch = 4;
-  if(thisPitch < 0) thisPitch = 0;
-  note(arr[thisPitch]);
-  playnote(amplitude);
+  playnote(sensorReading, amplitude);
   setx(wavelength / 10.0);
 }
 
@@ -38,16 +35,10 @@ void note(float key) {
   mstep = abs(curr - target) / 5;
 }
 
-void playnote(int amp) {
-  double y = curr * 4 + sin(x) * amp;
+void playnote(int note, int amp) {
+  double y = note * 1 + sin(x) * amp;
   Serial.println(y);
   tone(9,y, 100);
-  if(curr > target && curr - mstep > target)
-    curr -= mstep;
-  else if(curr < target && curr + mstep < target)
-    curr += mstep;
-  else
-    curr = target;
   delay(20); 
 }
 
